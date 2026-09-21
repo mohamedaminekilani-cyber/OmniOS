fcd89131db5cd8328d01c4d97e91052d96cb14b5
mohamedaminekilani-cyber <mohamedamine.kilani@etudiant-enit.utm.tn>
2026-09-21 16:19:06 +0100

    Bump OmniOS service worker cache to v62.3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='omnios-v62.2-static-pwa';
+const CACHE='omnios-v62.3-static-pwa';
 const CORE=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==CACHE)await caches.delete(k);await self.clients.claim()})())});

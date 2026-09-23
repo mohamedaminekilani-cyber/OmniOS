e87b047ad36f0d1f7572e6514d97123aa0faf127
mohamedaminekilani-cyber <mohamedamine.kilani@etudiant-enit.utm.tn>
2026-09-23 18:14:50 +0100

    Bump service worker cache for consolidated UX

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='omnios-v62.3-stable-startup-r4';
+const CACHE='omnios-v63-consolidated-ux-r1';
 const USER_ASSET_CACHE='omnios-user-assets-v1';
 const CORE=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./pwa-personalization.js'];
 
